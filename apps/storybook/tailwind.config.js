ec2ed0438deaeeaa8d3630aeaee4194965abc0c3
Tobias Meixner <meixner.tobias@gmail.com>
2024-12-17 14:39:06 +0700

    chore: remove tailwind pkg

--- apps/storybook/tailwind.config.js
+++ apps/storybook/tailwind.config.js
@@ -1,9 +1,1 @@
-// export default {
-//   presets: [require('@hubql/tailwind-config/tailwind')],
-//   content: [
-//     './src/**/*.{js,ts,jsx,tsx}',
-//     './stories/**/*.{js,ts,jsx,tsx}',
-//     '../../packages/ui/src/**/*.{js,ts,jsx,tsx}',
-//   ],
-// }
 export * from '@hubql/ui/tailwind.config'
