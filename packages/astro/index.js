123f8b7855f3e7af62a5a189be9c5ccff8bbc4dd
Tobias Meixner <meixner.tobias@gmail.com>
2024-12-25 02:45:00 +0700

    fix: ui import

--- packages/astro/index.js
+++ packages/astro/index.js
@@ -3,27 +3,21 @@ import matter from 'gray-matter'
 import tailwind from '@astrojs/tailwind'
 import fs from 'fs'
 import path from 'path'
+import plugin from 'tailwindcss'
 
 export default function hubqlIntegration() {
   return {
-    name: 'hubql-astro',
+    name: '@hubql/astro',
     hooks: {
       'astro:config:setup': ({ config, injectRoute, updateConfig }) => {
         // if (!options.contentDir) {
         //   throw new Error('Missing required contentDir')
         // }
-        const integrations = [tailwind(), mdx()]
+        const integrations = [mdx()]
         updateConfig({
           integrations: integrations,
           vite: {
-            resolve: {
-              alias: {
-                '@hubql/ui': path.resolve(process.cwd(), 'node_modules/@hubql/ui'),
-              },
-              extensions: ['.js', '.ts', '.jsx', '.tsx', '.json'],
-            },
             optimizeDeps: {
-              include: ['react', 'react-dom'],
               exclude: ['@hubql/ui'],
             },
           },
